77bbe5918db0b2cc51251aac19f5cbbbe2624012
JTilley24 <justin.tilley@me.com>
2013-01-31 22:00:36 -0500

    Project 4 complete

--- Project 3/asdcouch/app/_attachments/main.js
+++ Project 3/asdcouch/app/_attachments/main.js
@@ -280,6 +280,8 @@ $("#additem").on("pageinit", function(){
 
 
 });
+
+
 var urlVars = function(){
 	var urlData = $($.mobile.activePage).data("url")
 	var urlParts = urlData.split("?");
@@ -290,10 +292,12 @@ var urlVars = function(){
 		var key = decodeURIComponent(keyValue[0]);
 		var value = decodeURIComponent(keyValue[1]);
 		urlValues[key] = value;
+		var urlregion = key
 	}
-	return urlValues;
+	return [urlValues, urlregion];
 };
 
+//Domestic Vehicles
 $("#domrepair").live("pageshow", function(){
 	
 	//Dynamic Couch data
@@ -314,10 +318,12 @@ $("#domrepair").live("pageshow", function(){
 	
 });
 
+//Domestic Detail
 $("#domdetails").live("pageshow", function(){
 	
-	var dominfo = urlVars()["dom"];
-	//console.log(dominfo);
+	var dominfo = urlVars()[0]["dom"];
+	console.log(urlVars());
+	console.log(dominfo);
 	var domitem;
 	$.couch.db("asdproject").view("vehiclerepair/dom", {
 			success: function(data) {
@@ -326,24 +332,285 @@ $("#domdetails").live("pageshow", function(){
 					var item = (value.value || value.doc);
 					if(item.model === dominfo){
 						domitem = item;
+						domVal = value;
 					};
 				});
 				$("#domDlist").append(
-					$("<li>").text(
-						"Region: "+domitem.region
-						+"Make: "+domitem.make
-						+"Model: "+domitem.model
-						+"Parts: "+domitem.parts
-						+"Year: "+domitem.year
-						+"Date: "+domitem.date
-						+"Comments: "+domitem.comments
-					)
+						$("<li>").text("Region: "+domitem.region),
+						$("<li>").text("Make: "+domitem.make),
+						$("<li>").text("Model: "+domitem.model),
+						$("<li>").text("Parts: "+domitem.parts),
+						$("<li>").text("Year: "+domitem.year),
+						$("<li>").text("Date: "+domitem.date),
+						$("<li>").text("Comments: "+domitem.comments)
 				)
-				$("#domDlist").listview("refresh");
+				$("domDlist").listview("refresh");
+				$("#editLink").attr("href", "form.html?dom="+domitem.model);
+				$("#deleteLink").on("click", function(){
+						deleteDoc(domVal);
+				})
 			}
 	});	
 });
 
+//European Vehicles
+$("#eurorepair").live("pageshow", function(){
+	
+	//Dynamic Couch data
+	$.couch.db("asdproject").view("vehiclerepair/euro", {
+			success: function(data) {
+				$("#eurolist").empty();
+				$.each(data.rows, function(index, value){
+					var item = (value.value || value.doc);
+					$("#eurolist").append(
+						$("<li>").append(
+							$("<a>").attr("href", "euro.html?euro=" + item.model).text(item.year+" "+item.make+" "+item.model)
+						)
+					);
+				});
+				$("#eurolist").listview("refresh");
+			}
+	});	
+	
+});
+
+//European Details
+$("#eurodetails").live("pageshow", function(){
+	
+	var euroinfo = urlVars()[0]["euro"];
+	console.log(euroinfo);
+	var euroitem;
+	$.couch.db("asdproject").view("vehiclerepair/euro", {
+			success: function(data) {
+				$("#euroDlist").empty();
+				$.each(data.rows, function(index, value){
+					var item = (value.value || value.doc);
+					if(item.model === euroinfo){
+						euroitem = item;
+						euroVal = value;
+					};
+				});
+				$("#euroDlist").append(
+						$("<li>").text("Region: "+euroitem.region),
+						$("<li>").text("Make: "+euroitem.make),
+						$("<li>").text("Model: "+euroitem.model),
+						$("<li>").text("Parts: "+euroitem.parts),
+						$("<li>").text("Year: "+euroitem.year),
+						$("<li>").text("Date: "+euroitem.date),
+						$("<li>").text("Comments: "+euroitem.comments)
+				)
+				$("euroDlist").listview("refresh");
+				$("#editLink").attr("href", "form.html?euro="+euroitem.model);
+				$("#deleteLink").on("click", function(){
+						deleteDoc(euroVal);
+				})
+			}
+	});	
+});
+
+//Asian Vehicles
+$("#asiarepair").live("pageshow", function(){
+	
+	//Dynamic Couch data
+	$.couch.db("asdproject").view("vehiclerepair/asia", {
+			success: function(data) {
+				$("#asialist").empty();
+				$.each(data.rows, function(index, value){
+					var item = (value.value || value.doc);
+					$("#asialist").append(
+						$("<li>").append(
+							$("<a>").attr("href", "asia.html?asia=" + item.model).text(item.year+" "+item.make+" "+item.model)
+						)
+					);
+				});
+				$("#asialist").listview("refresh");
+			}
+	});	
+	
+});
+
+//Asian Detail
+$("#asiadetails").live("pageshow", function(){
+	
+	var asiainfo = urlVars()[0]["asia"];
+	console.log(asiainfo);
+	var asiaitem;
+	$.couch.db("asdproject").view("vehiclerepair/asia", {
+			success: function(data) {
+				$("#asiaDlist").empty();
+				$.each(data.rows, function(index, value){
+					var item = (value.value || value.doc);
+					if(item.model === asiainfo){
+						asiaitem = item;
+						asiaVal = value;
+					};
+				});
+				$("#asiaDlist").append(
+						$("<li>").text("Region: "+asiaitem.region),
+						$("<li>").text("Make: "+asiaitem.make),
+						$("<li>").text("Model: "+asiaitem.model),
+						$("<li>").text("Parts: "+asiaitem.parts),
+						$("<li>").text("Year: "+asiaitem.year),
+						$("<li>").text("Date: "+asiaitem.date),
+						$("<li>").text("Comments: "+asiaitem.comments)
+				)
+				$("asiaDlist").listview("refresh");
+				$("#editLink").attr("href", "form.html?asia="+asiaitem.model);
+				$("#deleteLink").on("click", function(){
+						deleteDoc(asiaVal);
+				})
+			}
+	});	
+});
+
+//Add New Document function
+var addDoc = function(){
+		 var doc = {};
+		 var radios = $("input:radio[name=newregion]");
+			if(radios[0].checked){
+				itemId = "dom:";
+				doc.region = "Domestic"
+			}else if(radios[1].checked){
+				itemId = "euro:";
+				doc.region = "European"
+			}else if(radios[2].checked){
+				itemId = "asia:";
+				doc.region = "Asian"
+			}
+		console.log(doc);
+		doc.make = $("#newmake").val();
+		doc.model = $("#newmodel").val();
+		doc.year = $("#newyear").val();
+		doc.parts = $("#newparts").val();
+		doc.date = $("#newdate").val();
+		doc.comments = $("#newcomments").val();
+		doc["_id"]=itemId+":"+doc.model;
+		$.couch.db("asdproject").saveDoc(doc, {
+			success: function(data){
+				console.log(data);
+				alert("Repair Saved");
+				$("#formreset").click();
+			},
+			error: function(status){
+				console.log(status);
+			}
+		});
+}
+
+//Edit Document function
+var updateDoc = function(value){
+		$("#editSubmit")
+		console.log(value.id);
+		$.couch.db("asdproject").openDoc(value.id, {
+			success: function(data){
+				var radios = $("input:radio[name=editregion]");
+				if(radios[0].checked){
+					data.region = "Domestic"
+				}else if(radios[1].checked){
+					data.region = "European"
+				}else if(radios[2].checked){
+					data.region = "Asian"
+				}
+				data.make = $("#editmake").val();
+				data.model = $("#editmodel").val();
+				data.year = $("#edityear").val();
+				data.parts = $("#editparts").val();
+				data.date = $("#editdate").val();
+				data.comments = $("#editcomments").val();
+				console.log(data);
+				$.couch.db("asdproject").saveDoc(data, {
+					success: function(data){
+						console.log(data);
+					},
+					error: function(status){
+						console.log(status);
+					}
+				})
+			}
+		})		
+}
+
+//Delete Document function
+var deleteDoc = function(value){
+	$.couch.db("asdproject").openDoc(value.id, {
+			success: function(data){
+				var doc = {
+					_id: data._id,
+					_rev: data._rev
+				}
+				console.log(doc);
+				var ask = confirm("Are you sure?");
+				if(ask){
+					alert("Repair was deleted.");
+					$.couch.db("asdproject").removeDoc(doc, {
+						success: function(data){
+							console.log(data);
+							$.mobile.changePage($("#repairs"));
+						},
+						error: function(status){
+							console.log(status);
+						}
+					});
+				}else{
+					alert("Repair was NOT deleted.");
+					
+				}
+			}
+	});
+};
+
+
+$("#editRepair").live("pageshow", function(){
+	$("#formSubmit").attr("id", "editsubmit");
+	var urlRegion = urlVars()[1];
+	var urlItem = urlVars()[0][urlRegion];
+	var item;
+	console.log(urlRegion);
+	console.log(urlItem);
+	$.couch.db("asdproject").view("vehiclerepair/"+urlRegion, {
+		success: function(data) {
+			console.log(data);
+			$.each(data.rows, function(index, value){
+					var docItem = (value.value || value.doc);
+					if(docItem.model === urlItem){
+						console.log(value);
+						item = docItem;
+						console.log(item);
+					};
+			});
+			$("#editmake").val(item.make);
+			$("#editmodel").val(item.model);
+			$("#edityear").val(item.year);
+			$("#editparts").val(item.parts);
+			$("#editdate").val(item.date);
+			$("#editcomments").val(item.comments);
+			$("#editparts").selectmenu("refresh");
+			var radios = $("input:radio[name=editregion]");
+			if(item.region == "Domestic"){
+				radios[0].click();
+			}else if(item.region == "Asian"){
+				radios[2].click();
+			}else if(item.region == "European"){
+				radios[1].click();
+			}
+			$.each(data.rows, function(index, value){
+				if(item == value.value){
+					$("#editsubmit").on("click", function(){
+						updateDoc(value);
+						alert("Repair saved.")
+						$.mobile.changePage($("#repairs"));
+					})
+				}		
+			})
+		}
+	});
+});
+
+$("#newRepair").live("pageshow", function(){
+	$("#formsubmit").on("click", function(){
+		addDoc();
+	})
+})
 
 $("#engine").on("pageinit", function(){
 
